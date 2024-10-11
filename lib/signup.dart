d34e78d833ad615ddf06b63e735d5753e6647d22
luizaki <francinelouise.sanchez@gmail.com>
2024-10-12 01:39:09 +0800

    encoded background

--- lib/signup.dart
+++ lib/signup.dart
@@ -15,115 +15,114 @@ class Signup extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('background/background.png'),
-            fit: BoxFit.cover
-          )
-        ),
+        decoration: AppBackground.mainBackground,
+        // decoration: const BoxDecoration(
+        //     image: DecorationImage(
+        //         image: AssetImage('assets/background/background.png'),
+        //         fit: BoxFit.cover)),
         child: Stack(
           children: [
-          Dialog(
-          insetPadding:
-              const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                GradientText("Create account",
-                    style: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Outfit'
+            const BackgroundTest(colored: true),
+            DialogBackground(
+              //   insetPadding:
+              //       const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GradientText("Create account",
+                        style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Outfit'),
+                        gradient: LinearGradient(stops: const [
+                          0.61,
+                          1.00
+                        ], colors: [
+                          Theme.of(context).colorScheme.primary,
+                          Theme.of(context).colorScheme.secondary,
+                        ])),
+                    const SizedBox(height: 16.0),
+                    Theme(
+                      data: AppTheme.textFieldTheme,
+                      child: const Column(children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              fontFamily:
+                                  'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              fontFamily:
+                                  'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Confirm Password",
+                            labelStyle: TextStyle(
+                              fontFamily:
+                                  'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                      ]),
                     ),
-                    gradient: LinearGradient(stops: const [
-                      0.61,
-                      1.00
-                    ], colors: [
-                      Theme.of(context).colorScheme.primary,
-                      Theme.of(context).colorScheme.secondary,
-                    ])),
-                const SizedBox(height: 16.0),
-                Theme(
-                  data: AppTheme.textFieldTheme,
-                  child: const Column(children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          fontFamily:
-                              'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
+                    const SizedBox(height: 32.0),
+                    Theme(
+                      data: AppTheme.enterButtonTheme,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const Home(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(
+                              55), // Set the desired height
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        child: const Text(
+                          "Sign up",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily:
+                                'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          fontFamily:
-                              'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
-                        ),
-                      ),
-                      obscureText: true,
+                    const SizedBox(height: 0.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const Login(),
+                        ));
+                      },
+                      child: const Text("Already have an account? Login",
+                          style: TextStyle(fontFamily: 'Outfit')),
                     ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Confirm Password",
-                        labelStyle: TextStyle(
-                          fontFamily:
-                              'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                  ]),
+                  ],
                 ),
-                const SizedBox(height: 32.0),
-                Theme(
-                  data: AppTheme.enterButtonTheme,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const Home(),
-                      ));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize:
-                          const Size.fromHeight(55), // Set the desired height
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    child: const Text(
-                      "Sign up",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontFamily:
-                            'Outfit', //GoogleFonts.getFont('Satisfy').fontFamily,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 0.0),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const Login(),
-                    ));
-                  },
-                  child: const Text("Already have an account? Login",
-                      style: TextStyle(fontFamily: 'Outfit')),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
           ],
+        ),
       ),
-    ),
     );
   }
 }
