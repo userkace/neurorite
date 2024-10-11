4666650710d7a788cec6e58db765718ee20148f0
luizaki <francinelouise.sanchez@gmail.com>
2024-10-12 01:39:09 +0800

    encoded background

--- lib/login.dart
+++ lib/login.dart
@@ -16,108 +16,109 @@ class Login extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('background/background.png'),
-            fit: BoxFit.cover
-          )
-        ),
+        decoration: AppBackground.mainBackground,
+        // decoration: const BoxDecoration(
+        //     image: DecorationImage(
+        //         image: AssetImage('assets/background/background.png'),
+        //         fit: BoxFit.cover)),
         child: Stack(
           children: [
-            Dialog(
-          insetPadding:
-              const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                GradientText("Neurorite",
-                    style: const TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                      fontFamily:
-                          'Satisfy', //GoogleFonts.getFont('Satisfy').fontFamily,
+            const BackgroundTest(colored: true),
+            DialogBackground(
+              //   insetPadding:
+              //       const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GradientText("Neurorite",
+                        style: const TextStyle(
+                          fontSize: 70,
+                          fontWeight: FontWeight.bold,
+                          fontFamily:
+                              'Satisfy', //GoogleFonts.getFont('Satisfy').fontFamily,
+                        ),
+                        gradient: LinearGradient(stops: const [
+                          0.61,
+                          1.00
+                        ], colors: [
+                          Theme.of(context).colorScheme.primary,
+                          Theme.of(context).colorScheme.secondary,
+                        ])),
+                    const SizedBox(height: 16.0),
+                    Theme(
+                      data: AppTheme.textFieldTheme,
+                      child: const Column(children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              fontFamily:
+                                  'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              fontFamily:
+                                  'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
+                            ),
+                          ),
+                          obscureText: true, // Hide password
+                        ),
+                      ]),
                     ),
-                    gradient: LinearGradient(stops: const [
-                      0.61,
-                      1.00
-                    ], colors: [
-                      Theme.of(context).colorScheme.primary,
-                      Theme.of(context).colorScheme.secondary,
-                    ])),
-                const SizedBox(height: 16.0),
-                Theme(
-                  data: AppTheme.textFieldTheme,
-                  child: const Column(children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
+                    const SizedBox(height: 32.0),
+                    Theme(
+                      data: AppTheme.enterButtonTheme,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const Home(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(
+                              55), // Set the desired height
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily:
+                                'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 0.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const Signup(),
+                        ));
+                      },
+                      child: const Text(
+                        "Don't have an account? Sign up",
+                        style: TextStyle(
                           fontFamily:
                               'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
                         ),
                       ),
                     ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          fontFamily:
-                              'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
-                        ),
-                      ),
-                      obscureText: true, // Hide password
-                    ),
-                  ]),
+                  ],
                 ),
-                const SizedBox(height: 32.0),
-                Theme(
-                  data: AppTheme.enterButtonTheme,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const Home(),
-                      ));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize:
-                          const Size.fromHeight(55), // Set the desired height
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontFamily:
-                            'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 0.0),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const Signup(),
-                    ));
-                  },
-                  child: const Text(
-                    "Don't have an account? Sign up",
-                    style: TextStyle(
-                      fontFamily:
-                          'Outfit', //GoogleFonts.getFont('Outfit').fontFamily,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )],
-      ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/theme/background.dart
+++ lib/theme/background.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppBackground {
@@ -13,21 +14,98 @@ class AppBackground {
   );
 
   static Widget coloredCircleBg = Positioned(
-              top: -163,
-              left: -100,
-              child: Container(
-                height: 775,
-                width: 762,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFFE99C9C),
-                      Color(0xFFA872FF)
-                    ]
-                  ),
+      top: -163,
+      left: -100,
+      child: Container(
+        height: 775,
+        width: 762,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFFE99C9C), Color(0xFFA872FF)]),
+        ),
+      ));
+}
+
+class BackgroundTest extends StatelessWidget {
+  final bool colored;
+
+  const BackgroundTest({this.colored = true});
+
+  @override
+  Widget build(BuildContext context) {
+    final mainEllipseWidth = MediaQuery.of(context).size.width * (562 / 412);
+    final mainEllipseHeight = MediaQuery.of(context).size.height * (575 / 917);
+    final mainEllipseTopOffset =
+        MediaQuery.of(context).size.height * (-163 / 917);
+
+    return Stack(children: [
+      Positioned(
+          top: mainEllipseTopOffset -
+              (((mainEllipseHeight * 1.15) - mainEllipseHeight) / 2),
+          left: (MediaQuery.of(context).size.width / 2) -
+              (MediaQuery.of(context).size.width * (562 / 412) * 1.15 / 2),
+          child: Container(
+            width: mainEllipseWidth * 1.15,
+            height: mainEllipseHeight * 1.15,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0xFF787DF6), Color(0xFF565CFF)]),
+            ),
+          )),
+      Positioned(
+          top: mainEllipseTopOffset,
+          left:
+              (MediaQuery.of(context).size.width / 2) - (mainEllipseWidth / 2),
+          child: Container(
+            width: mainEllipseWidth,
+            height: mainEllipseHeight,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0xFFE99C9C), Color(0xFFA872FF)]),
+            ),
+          ))
+    ]);
+  }
+}
+
+class DialogBackground extends StatelessWidget {
+  final Widget? child;
+
+  const DialogBackground({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ClipRRect(
+        borderRadius:
+            BorderRadius.circular(15.0), // Rounded corners for the dialog
+        child: BackdropFilter(
+          filter:
+              ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0), // Blurring effect
+          child: Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: MediaQuery.of(context).size.height - 150,
+              decoration: BoxDecoration(
+                color: const Color(0x54FFFFFF), // Semi-transparent background
+                borderRadius:
+                    BorderRadius.circular(20.0), // Matching rounded corners
+                border: Border.all(
+                  width: 2,
+                  color: const Color(0x1aF1F1F1), // Border for frosted look
                 ),
-              ));
+              ),
+              child: child),
+        ),
+      ),
+    );
+  }
 }
